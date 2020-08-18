3a8ded2744b9817a8ec00462e0ba9c204b96bcde
szyar <soezayarszyar@gmail.com>
2020-08-18 21:01:33 +0630

    finish new post functionality with confirmation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,32 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        flash[:notice] = "post Created"
+        redirect_to @post
+      else
+        render 'new'
+      end
+    end
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    @post.user = current_user
+    render 'new' if @post.invalid?
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :picture, :picture_cache, :user_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :posts
-  
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
+
   root "sessions#new"
 
   get 'signup', to: 'users#new'
