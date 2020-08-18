e9ea32627b2fcecb610bdfccc1da022f820dcb6e
szyar <soezayarszyar@gmail.com>
2020-08-18 11:50:46 +0630

    update signup functionality with profile field

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
     "Instagram"
   end
 
+  def index
+    @users = User.all
+  end
+
+  def show
+    
+  end
 
   def new
     @user = User.new
@@ -18,6 +25,12 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
