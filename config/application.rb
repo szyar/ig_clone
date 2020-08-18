ef10b5f5b6af12a5418fcf5740a630c0631fdfef
szyar <soezayarszyar@gmail.com>
2020-08-18 09:32:31 +0630

    set configuration according to diver

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,16 @@ Bundler.require(*Rails.groups)
 
 module IgClone
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators do |g|
+      # These two lines describe the settings that are not automatically generated.
+      g.Assets false
+      g.Helper false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
