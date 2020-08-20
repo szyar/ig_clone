3a94495602ed70bb51d98c08bba6df942de7b56f
szyar <soezayarszyar@gmail.com>
2020-08-20 19:03:21 +0630

    implement letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
 end
